79ce49a91a23409d49e3fd991e8b9a1a2f10077e
rglang <rqlang@outlook.com>
2026-03-26 13:54:14 +0100

    Refactor path display in collect_declared_variable_errors to use format_path function

--- cli/src/syntax/resolve.rs
+++ cli/src/syntax/resolve.rs
@@ -842,7 +842,7 @@ pub fn collect_declared_variable_errors(
                         line,
                         col,
                         0..0,
-                        path.display().to_string(),
+                        format_path(&path),
                     ));
                 }
             }
